5a2fd1013f9cc4fb76b1e76fc16103eaae3ceaf5
natibi121 <natibi279@gmail.com>
2022-11-16 12:00:50 +0100

    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { BrowserRouter, NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { InstrumentListPage} from"./InstrumentListPage";
+import { InstrumentSinglePage} from"./InstrumentSinglePage";
+import { InstrumentCreatePage} from"./InstrumentcreatePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+  <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+      <li className="nav-item">
+        <NavLink to={`/`} activeClassName="active" exact>
+        <span className="nav-link">Hangszerek</span>
+        </NavLink>
+      </li>
+      <li className="nav-item">
+      <NavLink to={`/uj-hangszer`} activeClassName="active" active>
+      <span className="nav-link">Új hangszer</span>
+      </NavLink>  
+      </li>
+      </ul>
     </div>
+  </nav>
+  <Switch>
+    <Route path="/" exact component={InstrumentListPage } />
+  <Route path="/" exact>
+    Hangszer listaoldal
+  </Route>
+  <Route path="/hangszer/:hangszerId" element={InstrumentSinglePage}>
+    Hangszer aloldal
+  </Route>
+  <Route path="/uj-hangszer">
+    Új hangszer létrehozó oldal
+  </Route>
+  <Redirect to={"/"} />
+  </Switch>
+  </BrowserRouter>
   );
 }
 
